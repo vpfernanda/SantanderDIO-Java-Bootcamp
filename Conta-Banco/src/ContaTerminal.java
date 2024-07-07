c81125a47606f0dabe5607eac2231e68c477d871
vpfernanda <fernandavpagano@gmail.com>
2024-07-07 12:00:20 -0300

    Updated README.md - Explanations about "Conta-Banco" project.

---
+++ Conta-Banco/src/ContaTerminal.java
@@ -0,0 +1,34 @@
+public class ContaTerminal {
+    private int accountNumber; 
+    private String agencyCode;
+    private String clientName;
+    private double balance;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public String getAgencyCode() {
+        return agencyCode;
+    }
+    public void setAgencyCode(String agencyCode) {
+        this.agencyCode = agencyCode;
+    }
+    public String getClientName() {
+        return clientName;
+    }
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    
+
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SantanderDIO-Java-Bootcamp
 Repository created to store the files used during my learning journey in Java BackEnd Bootcamp provided by Santander &amp; Digital Innovation One's partnership.  This repository is for educational purposes only. 
 
-# First Change
-First change to be comitted. 
+# Conta-Banco
+This project was made as an exercise to practice Java Syntax. It was proposed as a part of Dio&Santander's Java Backend Bootcamp.
 
